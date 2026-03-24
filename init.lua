c0ced7612c4a4be74d2604472c3d69a67bfd75e8
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2026-03-24 08:33:57 +0100

    Stop using relative line numbers

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ require("config.lazy")
 
 -- Line numbers without relative numbering (easier to read for most people)
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 
 -- Smart case-insensitive search (lowercase = ignore case, uppercase = match case)
 vim.opt.ignorecase = true
