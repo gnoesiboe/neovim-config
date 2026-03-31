750ab115399efb43d701e60f35788bf8a5c64d7a
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2026-03-31 19:11:51 +0200

    Show explorer normally, but wider, to see more

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,8 +13,16 @@ return {
             sources = {
                 explorer = {
                     -- Show explorer as modal instead of sidebar
-                    layout = { preset = "default", preview = true },
-                    auto_close = true, -- 👈 closes after selecting a file
+                    -- layout = { preset = "default", preview = true },
+                    -- auto_close = true, -- 👈 closes after selecting a file
+
+                    -- Change width of explorer
+                    layout = {
+                        layout = {
+                            width = 70, -- set your desired width here
+                            min_width = 70, -- optional: prevent it from going narrower
+                        },
+                    },
                 },
             },
         },
