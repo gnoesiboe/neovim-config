81932ae38788f6c53f8feb1d41acba1e62ae4a89
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2026-04-20 23:12:57 +0200

    Change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,8 @@ return {
         "LazyVim/LazyVim",
         opts = {
             -- Set the theme to one of the installed in other plugin files
-            colorscheme = "kanagawa-wave",
-            -- colorscheme = "catppuccin-mocha",
+            -- colorscheme = "kanagawa-wave",
+            colorscheme = "catppuccin-mocha",
         },
     },
 }
