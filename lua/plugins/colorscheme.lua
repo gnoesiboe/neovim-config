f06a49da4f754b1cb6902674ee82ac3d1c97af77
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2026-05-07 14:08:55 +0200

    Add some new themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,9 @@ return {
         "LazyVim/LazyVim",
         opts = {
             -- Set the theme to one of the installed in other plugin files
+            colorscheme = "terafox",
             -- colorscheme = "kanagawa-wave",
-            colorscheme = "catppuccin-mocha",
+            -- colorscheme = "catppuccin-mocha",
         },
     },
 }
