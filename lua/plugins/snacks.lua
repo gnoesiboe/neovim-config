ca7027874a26d8742efb5dd0b587e99f779a38de
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2026-05-16 22:29:06 +0200

    Use ivy layout for explorer as well

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,12 +20,13 @@ return {
                     auto_close = true, -- 👈 closes after selecting a file
 
                     -- Change width of explorer
-                    layout = {
-                        layout = {
-                            width = 70, -- set your desired width here
-                            min_width = 70, -- optional: prevent it from going narrower
-                        },
-                    },
+                    layout = "ivy",
+                    -- layout = {
+                    --     layout = {
+                    --         width = 70, -- set your desired width here
+                    --         min_width = 70, -- optional: prevent it from going narrower
+                    --     },
+                    -- },
                 },
             },
 
