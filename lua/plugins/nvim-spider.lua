04e6288dc4584a4a4313d1174ebd48546df3cc4b
Gijs Nieuwenhuis <gijsnieuwenhuis@proton.me>
2026-05-28 22:43:09 +0200

    Re-enable spider for sub word movements when camel case

--- lua/plugins/nvim-spider.lua
+++ lua/plugins/nvim-spider.lua
@@ -1,10 +1,13 @@
 return {
+    "chrisgrieser/nvim-spider",
     --     -- This plugin allows be to use the word keys to jump between parts of words instead of entire words
-    --     "chrisgrieser/nvim-spider",
-    --     keys = {
-    --         { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
-    --         { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
-    --         { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
-    --         { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
-    --     },
+    keys = {
+        { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+        { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+        { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+        { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+    },
+    opts = {
+        skipInsignificantPunctuation = false,
+    },
 }
