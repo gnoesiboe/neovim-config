8e8c97c7f66be0d86cda66087ebc2eba475c7567
Gijs Nieuwenhuis <gijsnieuwenhuis@proton.me>
2026-05-28 22:42:55 +0200

    Change color schema to kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,8 @@ return {
         "LazyVim/LazyVim",
         opts = {
             -- Set the theme to one of the installed in other plugin files
-            colorscheme = "terafox",
-            -- colorscheme = "kanagawa-wave",
+            -- colorscheme = "terafox",
+            colorscheme = "kanagawa-wave",
             -- colorscheme = "catppuccin-mocha",
         },
     },
