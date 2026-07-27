d90981139ac0fe71c1bd849c2594e9258c875e36
Gijs Nieuwenhuis <gijsnieuwenhuis@proton.me>
2026-07-27 08:39:09 +0200

    Sidebar with code outline on the right

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,16 @@
+// Plugin that shows code structure outline in the right sidebar with `<space> c s`
+return {
+    "stevearc/aerial.nvim",
+    opts = {
+        filter_kind = {
+            "Class",
+            "Constructor",
+            "Function",
+            "Interface",
+            "Method",
+            -- "Enum",
+            -- "Module",
+            -- "Struct",
+        },
+    },
+}
