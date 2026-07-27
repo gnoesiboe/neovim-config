17b93b5960bfa627c24494d09f88425ff4b82050
Gijs Nieuwenhuis <gijsnieuwenhuis@proton.me>
2026-07-27 08:40:41 +0200

    More Neovide keyboard tuning

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -43,3 +43,23 @@ vim.keymap.set("n", "dd", [["_dd]], { noremap = true })
 
 -- Write buffer and delete it in one go
 vim.keymap.set("n", "<leader>wd", "<cmd>w<cr><cmd>bd<cr>", { desc = "Write and delete buffer" })
+
+-- For Neovide, as not using Kitty there
+if vim.g.neovide then
+    local function save()
+        vim.cmd.write()
+    end
+    local function copy()
+        vim.cmd([[normal! "+y]])
+    end
+    local function paste()
+        vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+    end
+
+    vim.keymap.set({ "n", "i", "v" }, "<D-s>", save, { desc = "Save" })
+    vim.keymap.set("v", "<D-c>", copy, { silent = true, desc = "Copy" })
+    vim.keymap.set({ "n", "i", "v", "c", "t" }, "<D-v>", paste, { silent = true, desc = "Paste" })
+
+    -- Make moving lines up an dodwn work in Neovide
+    vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+end
