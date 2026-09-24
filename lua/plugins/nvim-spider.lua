bf821d7c20120c702f0e673b140bd022caac97de
Gijs Nieuwenhuis <gijsnieuwenhuis@proton.me>
2026-09-24 11:12:12 +0200

    Disabled NVIM spider for now

--- lua/plugins/nvim-spider.lua
+++ lua/plugins/nvim-spider.lua
@@ -1,14 +1,16 @@
-return {
-    "chrisgrieser/nvim-spider",
-    --     -- This plugin allows be to use the word keys to jump between parts of words instead of entire words
-    keys = {
-        { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
-        { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
-        { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
-        { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
-    },
-    opts = {
-        -- Fixes strange behavior skipping humps of special characters
-        skipInsignificantPunctuation = false,
-    },
-}
+return {}
+
+-- return {
+--     "chrisgrieser/nvim-spider",
+--     --     -- This plugin allows be to use the word keys to jump between parts of words instead of entire words
+--     keys = {
+--         { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+--         { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+--         { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+--         { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+--     },
+--     opts = {
+--         -- Fixes strange behavior skipping humps of special characters
+--         skipInsignificantPunctuation = false,
+--     },
+-- }
