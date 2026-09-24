1d7b1c25e6c6792fc83ffaf962eee9b0703b1465
Gijs Nieuwenhuis <gijsnieuwenhuis@proton.me>
2026-09-24 11:12:00 +0200

    Remove concealed code blocks from markdown rendering

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+        code = {
+            style = "none", -- don't conceal/replace code fence lines
+        },
+        anti_conceal = { enabled = true },
+    },
+}
